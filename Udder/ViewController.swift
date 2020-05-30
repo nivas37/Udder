cdd62b5ad9bafc0a7a6f8a400f70513ed9ccf464
Nivas Ramanathan <nivas37@gmail.com>
2020-05-30 15:07:53 +0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Nivas Ramanathan on 30/05/2020.
+//  Copyright © 2020 Nivas Ramanathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
